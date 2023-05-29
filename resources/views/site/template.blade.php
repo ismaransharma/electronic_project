36611c849145a22d6cfd70eb78aaf9e187cb7ec0
ismaran sharma <ismaransharma.14@gmail.com>
2023-05-29 10:14:26 +0545

    main

---
+++ resources/views/site/template.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electronic | E-Commerce</title>
+
+    <!-- bootstrap ko css link gareko -->
+    <link rel="stylesheet" href="{{ asset('site/bootstrap/bootstrap.css') }}">
+
+    <!-- fontawesome ko css link gareko -->
+    <link rel="stylesheet" href="{{ asset('site/fontawesome/all.css') }}">
+
+    <!-- Css link gareko -->
+    <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
+</head>
+<body>  
+
+    <section class="top-header">
+        <div class="top-header-padding">
+            
+        </div>
+    </section>
+
+
+
+
+
+
+
+    <!-- jquery link gareko -->
+    <script src="{{ asset('site/jquery/jquery.js') }}"></script>
+
+    <!-- bootstrap ko js link gareko -->
+    <script src="{{ asset('site/bootstrap/bootstrap.js') }}"></script>
+
+    <!-- fontawesome ko js link gareko -->
+    <script src="{{ asset('site/fontawesome/all.js')}}"></script>
+
+    <!-- Javascript link gareko -->
+    <script src="{{ asset('site/javascript/script.js') }}"></script>
+</body>
+</html>
