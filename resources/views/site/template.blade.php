4138963824b0ccb453e80f393548f33b45c8642b
ismaran sharma <ismaransharma.14@gmail.com>
2023-05-29 21:47:19 +0545

    top-header-finish

--- resources/views/site/template.blade.php
+++ resources/views/site/template.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,19 +16,41 @@
     <!-- Css link gareko -->
     <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
 </head>
-<body>  
 
-    <section class="top-header">
-        <div class="top-header-padding">
-            
+<body>
+
+    <section id="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-lg-8 col-xl-8 col-sm-12 col-12 text-sm-center">
+                    <div class="top-header-content">
+                        <div class="row">
+                            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6">
+                                <i class="fa-solid fa-location-dot icon"></i> <span class="top-header-text">Pokhara - 17, Birauta</span>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6">
+                                <i class="fa-solid fa-phone icon"></i> <class class="top-header-text">+977 9876543210</class>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-lg-4 col-xl-4 col-sm-12 col-12 text-end d-none d-md-block icons">
+                    <div class="top-header-social-icon">
+                        <a href="">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa-regular fa-envelope"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
-
-
-    <div class="footer">
-        abjga
-    </div>
-
 
 
     <!-- jquery link gareko -->
@@ -42,4 +65,5 @@
     <!-- Javascript link gareko -->
     <script src="{{ asset('site/javascript/script.js') }}"></script>
 </body>
+
 </html>
