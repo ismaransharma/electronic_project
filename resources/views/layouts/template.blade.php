de6a16213f0aee5cd783109ec1628bd5d3ecc77c
ismaran sharma <ismaransharma.14@gmail.com>
2023-06-04 00:01:47 +0545

    About Us Completed

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -28,11 +28,11 @@
                 <div class="col-md-8 col-lg-8 col-xl-12 col-sm-12 col-12 text-sm-center">
                     <div class="top-header-content">
                         <div class="row">
-                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 top-header-location-padding">
                                 <i class="fa-solid fa-location-dot icon"></i> <span class="top-header-text">Pokhara -
                                     17, Birauta</span>
                             </div>
-                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 number-padding">
                                 <i class="fa-solid fa-phone icon"></i>
                                 <class class="top-header-text">+977 9876543210</class>
                             </div>
@@ -60,27 +60,32 @@
     <!-- Top Header Ends Here -->
 
     <!-- navbar section starts here -->
-    <section id="header-navbar">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container text-center">
-                <a class="navbar-brand nav-title" href="#">Electronic </a>
+    <section id="top-header-navbar">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <h4>Gada Electronic</h4>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto text-center">
                         <li class="nav-item">
-                            <a class="nav-link active-nav" aria-current="page" href="#">Home</a>
+                            <a class="nav-link   active-nav" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">About Us</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Our Services</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
-                                Our Services
+                                Category
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Laptop</a></li>
@@ -89,7 +94,7 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contact Us</a>
+                            <a class="nav-link" aria-current="page" href="#">Contact Us</a>
                         </li>
                     </ul>
                 </div>
@@ -210,7 +215,8 @@
                             </div>
                         </div>
                     </div>
-                    <div class="slider-button">
+
+                    <!-- <div class="slider-button">
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
                             data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -221,10 +227,59 @@
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Slider Section Ends Here -->
+
+    <!-- About Section Starts Here -->
+    <section id="about" class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title">
+                        <h6>About Us</h6>
+                        <h2>Learn More <span>About Us</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="about-image">
+                        <img class="about-wala-image" src="{{ asset('site/images/about-section.jpg') }}"
+                            alt="ABOUT IMAGE" title="Gada Electronic" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="about-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deserunt porro veniam
+                            aperiam
+                            iure iste sed, ipsa vero architecto nam, harum, laudantium doloribus sunt. Soluta,
+                            architecto doloribus. Ipsum, eos omnis.
+
+                        </p>
+
+                        <p style="text-align: center">
+                            <a class="btn about-button">Read More</a>
+                        </p>
+                    </div>
+
+                    <div class="about-video">
+                        <iframe width="350px" height="268px" src="https://www.youtube.com/embed/_sx5QZA-jr8"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen>
+                        </iframe>
                     </div>
                 </div>
             </div>
         </div>
+    </section>
+
+    <!-- About Section Ends Here -->
 
 
 
@@ -247,7 +302,6 @@
 
 
 
-        <!-- Silder section ends Here -->
 
 
 
@@ -257,22 +311,20 @@
 
 
 
+    <!-- jquery link gareko -->
+    <script src="{{ asset('site/jquery/jquery.js') }}"></script>
 
+    <!-- popper javascript link gareko -->
+    <script src="{{ asset('site/bootstrap/popper.js') }}"></script>
 
-        <!-- jquery link gareko -->
-        <script src="{{ asset('site/jquery/jquery.js') }}"></script>
+    <!-- bootstrap ko js link gareko -->
+    <script src="{{ asset('site/bootstrap/bootstrap.js') }}"></script>
 
-        <!-- popper javascript link gareko -->
-        <script src="{{ asset('site/bootstrap/popper.js') }}"></script>
+    <!-- fontawesome ko js link gareko -->
+    <script src="{{ asset('site/fontawesome/all.js')}}"></script>
 
-        <!-- bootstrap ko js link gareko -->
-        <script src="{{ asset('site/bootstrap/bootstrap.js') }}"></script>
-
-        <!-- fontawesome ko js link gareko -->
-        <script src="{{ asset('site/fontawesome/all.js')}}"></script>
-
-        <!-- Javascript link gareko -->
-        <script src="{{ asset('site/javascript/script.js') }}"></script>
+    <!-- Javascript link gareko -->
+    <script src="{{ asset('site/javascript/script.js') }}"></script>
 </body>
 
 </html>
